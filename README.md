0bd1fdee5d8a36233a318e7240e5d789d709fa2e
onemira <mirabulgogi@gmail.com>
2019-03-08 14:11:33 -0500

    read.md updated

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# Realtor
+https://docs.google.com/presentation/d/1p3LiywjCBl_xVqNjcBORa98mtv2CmTSEV3lEGYygbMM/edit?usp=sharing
+
+My capstone project is to build a business website for my mother in law.
+She works as a realtor in Los Angeles area and has been seeking to have her own website for her clients.  
+This will be my dedication for somebody who has been offering great support in many ways since I moved to the US as my first building commercial website as a developer.
+
+The website will consist of 5 sections:
+-Title
+-Profile
+-Listings
+-Sold properties
+-Contact
+
+It will be built as SPA in React using React Router. Each detail will be shown once clicked. She will be able to CDUR the listings on her own side by using API that I will create.
--- src/App.js
+++ src/App.js
@@ -3,7 +3,53 @@ import HelloWorld from './components/HelloWorld'
 
 class App extends Component {
   render() {
-    return <HelloWorld />
+    return (
+      <>
+        <link
+          href="https://fonts.googleapis.com/css?family=Cinzel+Decorative|Raleway"
+          rel="stylesheet"
+        />
+        <div className="title">
+          <h1 className="fullName">Cindy Shiao </h1>
+          <h2>Realtor & Mother</h2>
+        </div>
+        <div className="profile">
+          <h2 className="box">Realtor</h2>
+          <img src="profile.png" alt="cindy shiao" />
+          <h2>Profile</h2>
+        </div>
+        <div className="listing">
+          <h2>Listings</h2>
+          <input className="box" type="image" />
+          <input className="box" type="image" />
+          <input className="box" type="image" />
+        </div>
+        <div className="soldProperty">
+          <h2>Property sold</h2>
+          <input type="image" />
+        </div>
+        <div className="contact">
+          <h1 className="getInTouch">GET IN TOUCH</h1>
+          <h2>Contact</h2>
+          <h3>address</h3>
+          <h3>email</h3>
+          <h3>phone</h3>
+          <form action="" method="">
+            Name <input type="text" name="name" />
+            <br />
+            Email <input type="text" name="email" />
+            <br />
+            Subject
+            <input type="text" name="subject" />
+            <br />
+            Message
+            <input type="text" name="message" />
+            <br />
+            <input type="submit" value="submit" />
+          </form>
+        </div>
+      </>
+    )
   }
 }
 
